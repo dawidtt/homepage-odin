e44b84b8277a5b3b8b297e4251a27f3e06133382
dawidtt <dawid.gorszka@gmail.com>
2025-02-18 20:00:23 +0100

    Style header for mobile design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./template.css";
+import dave from "./assets/pexels-olly-927022.jpg";
+
+const headerImg = document.querySelector(".header-img");
+const daveImg = document.createElement("img");
+daveImg.src = dave;
+daveImg.alt = "Dave Ochoa";
+headerImg.appendChild(daveImg);
