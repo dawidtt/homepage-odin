bf36b5a4f305aed9b1633f4fd8f4b8b350d51756
dawidtt <dawid.gorszka@gmail.com>
2025-02-19 17:59:55 +0100

    Create html for footer section

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import "./template.css";
 import dave from "./assets/pexels-olly-927022.jpg";
+import daveFooter from "./assets/pexels-olly-926390.jpg";
 
 const headerImg = document.querySelector(".header-img");
 const daveImg = document.createElement("img");
 daveImg.src = dave;
 daveImg.alt = "Dave Ochoa";
 headerImg.appendChild(daveImg);
+
+const footerImg = document.querySelector(".footer-img");
+const daveFooterImg = document.createElement("img");
+daveFooterImg.src = daveFooter;
+daveFooterImg.alt = "Dave Ochoa";
+footerImg.appendChild(daveFooterImg);
