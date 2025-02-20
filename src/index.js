7f8b0c0740d4a909a45e287f82608ac8b74dea01
dawidtt <dawid.gorszka@gmail.com>
2025-02-20 02:33:14 +0100

    Add improvment in design
    Different pictures in footer for different resolutions.
    Change min-width for tablet.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./template.css";
 import dave from "./assets/pexels-olly-927022.jpg";
 import daveFooter from "./assets/pexels-olly-926390.jpg";
+import daveFooterTablet from "./assets/pexels-olly-926390-tablet.jpg";
 
 const headerImg = document.querySelector(".header-img");
 const daveImg = document.createElement("img");
@@ -8,8 +9,19 @@ daveImg.src = dave;
 daveImg.alt = "Dave Ochoa";
 headerImg.appendChild(daveImg);
 
+const picture = document.createElement("picture");
+const sourceTablet = document.createElement("source");
+sourceTablet.srcset = daveFooterTablet;
+sourceTablet.media = "(min-width: 550px)";
+const sourceComputer = document.createElement("source");
+sourceComputer.srcset = daveFooter;
+sourceComputer.media = "(min-width: 800px)";
+picture.appendChild(sourceComputer);
+picture.appendChild(sourceTablet);
 const footerImg = document.querySelector(".footer-img");
 const daveFooterImg = document.createElement("img");
 daveFooterImg.src = daveFooter;
 daveFooterImg.alt = "Dave Ochoa";
-footerImg.appendChild(daveFooterImg);
+
+picture.appendChild(daveFooterImg);
+footerImg.appendChild(picture);
